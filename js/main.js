54419bcd936a3e24c75e050fae2140451ca861bd
msiller455 <msiller455@gmail.com>
2024-10-10 16:04:57 -0700

    add sayHello function

--- js/main.js
+++ js/main.js
@@ -1,3 +1,10 @@
 console.log('Hello World!')
 
 const name = "michael"
+
+
+// Write a function that console logs your name
+// Add and commit your change with a good brief message
+function sayHello() {
+  console.log(`Hello ${name}`)
+}
